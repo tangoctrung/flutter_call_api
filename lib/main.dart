4343664425f5a8134aa275f6d55833dd7d0ce893
TangoctrungTangoctrung <tangoctrung12123@gmail.com>
2022-04-17 16:16:13 +0700

    dev api

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -6,40 +9,20 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Xem thời tiết hôm nay'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +31,125 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  double nhietdo = 0;
+  double doam = 0;
+  double sucgio = 0;
+  String des  = "";
+  String city = "Chon thanh pho";
+  
+  void _changeCity() async {
+    String url_api = "";
+    switch (city) {
+      case 'Ha Noi': 
+        url_api = 'https://api.openweathermap.org/data/2.5/weather?q=hanoi&appid=024b890760f06546f2697f39cb56a895';
+        break;
+      case 'Ha Nam': 
+        url_api = 'https://api.openweathermap.org/data/2.5/weather?q=hanam&appid=024b890760f06546f2697f39cb56a895';
+        break;
+      case 'Hai Phong': 
+        url_api = 'https://api.openweathermap.org/data/2.5/weather?q=haiphong&appid=024b890760f06546f2697f39cb56a895';
+        break;
+      case 'Da Nang': 
+        url_api = 'https://api.openweathermap.org/data/2.5/weather?q=danang&appid=024b890760f06546f2697f39cb56a895';
+        break;
+      case 'Thua Thien Hue': 
+        url_api = 'https://api.openweathermap.org/data/2.5/weather?q=hue&appid=024b890760f06546f2697f39cb56a895';
+        break;
+      default:
+        return;
+    }
+    var client = http.Client();
+    var res = await client.get(
+      Uri.parse(url_api),
+      headers: { 'Content-Type': 'application/json'},
+    );
+    Map<String, dynamic> json = jsonDecode(res.body.toString());
+    
+    print(json);
 
-  void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      nhietdo = json["main"]["temp"] - 273.15;
+      doam = json["main"]["humidity"].toDouble();
+      sucgio = json["wind"]["speed"].toDouble();
+      des = json["weather"][0]["description"].toString();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const SizedBox(height: 30),
+            DropdownButton<String>(
+              value: city,
+              style: const TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+              ),
+              underline: Container(
+                height: 0,
+              ),
+              onChanged: (String? newValue) {
+                setState(() {
+                  city = newValue!;
+                });
+                _changeCity();
+              },
+              items: <String>[
+                'Chon thanh pho',
+                'Ha Noi',
+                'Ha Nam',
+                'Hai Phong',
+                'Da Nang',
+                'Thua Thien Hue'
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
+            const SizedBox(height: 30),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Thời tiết: ' + des.toString(),
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 30),
+            Text(
+              'Nhiệt độ: ' + nhietdo.ceil().toString() + ' độ C',
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 30),
+            Text(
+              'Độ ẩm: ' + doam.toString() + '%',
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 30),
+            Text(
+              'Sức gió: ' + sucgio.toString() + ' m/s',
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
